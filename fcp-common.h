26a0b41a26886e9ec845a04e9e98e1c9cbf47b3b
cosparks <colby.r.sparks@gmail.com>
2022-11-22 18:20:06 -0800

    fixes some names

--- fcp-common.h
+++ fcp-common.h
@@ -1,8 +1,8 @@
 /* Telemetry Macros */
 #define FC_BAUD 9600
 
-#define NUM_CELLS 17
-#define NUM_HEADERS 6
+#define FC_NUM_CELLS 17
+#define FC_NUM_HEADERS 6
 
 #define FC_HEADER_0 0x7
 #define FC_HEADER_1 0xD

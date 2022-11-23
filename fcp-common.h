5c9cb19cb87a8419b3bb2893f0a1d15e84bb05ed
cosparks <colby.r.sparks@gmail.com>
2022-11-22 18:16:15 -0800

    Adds telemetry transmission protocol macros

---
+++ fcp-common.h
@@ -0,0 +1,12 @@
+
+#define FC_BAUD 9600
+
+#define NUM_CELLS 17
+#define NUM_HEADERS 6
+
+#define FC_HEADER_0 0x7
+#define FC_HEADER_1 0xD
+#define FC_HEADER_2 0x11
+#define FC_HEADER_3 0x17
+#define FC_HEADER_4 0x1D
+#define FC_HEADER_5 0x1F

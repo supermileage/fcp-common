b6d6a0f8d785a93c6223062c2e5ce0e253d2f8f7
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2023-03-11 17:08:44 -0800

    19 cells board 1.0

--- src/fcp-common.h
+++ src/fcp-common.h
@@ -1,7 +1,12 @@
 /* Telemetry Macros */
 #define FC_BAUD 9600
 
-#define FC_NUM_CELLS 17
+#define CELL_ARRAY_SIZE 20
+#define LEADING_ZEROES 0
+#define TRAILING_ZEROES 1
+
+#define FC_NUM_CELLS CELL_ARRAY_SIZE - LEADING_ZEROES - TRAILING_ZEROES
+
 #define FC_NUM_HEADERS 6
 
 #define FC_HEADER_0 0x7
